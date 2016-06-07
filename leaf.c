1406e676f14f63e1a4017fb3eb1640eb12675510
mary25 <mary2511995@gmail.com>
2016-06-07 10:04:50 +0530

    Create leaf.c

---
+++ leaf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%4==0)
+       printf("the given year is leaf year");
+	else
+	   printf("the given year is not a leaf year");
+
+    return 0;
+
+}
