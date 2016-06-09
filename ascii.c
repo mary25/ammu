69c7c506a89ffba52a8ad3fd119a330eaedd54cd
mary25 <mary2511995@gmail.com>
2016-06-09 21:55:41 +0530

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+ {
+ 	int i,j;
+ 	printf("capital letters:\n");
+ 	for(i='A';i<='Z';i++)
+ 	  {
+ 	  	printf("%c=%d\n",i,i);
+ 	  }
+ 	  printf("small letters:\n");
+ 	  for(i='a';i<='z';i++)
+ 	  {
+ 	  	printf("%c=%d\n",i,i);
+ 	  }
+ 	  return 0;
+ 	  
+ }
